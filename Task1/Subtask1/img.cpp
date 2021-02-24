03316ea7c9147ede435e6aa3242ef14e5c4211cd
Shrey <pshrey795@gmail.com>
2021-02-24 13:26:27 +0530

    Warping added

--- Task1/Subtask1/img.cpp
+++ Task1/Subtask1/img.cpp
@@ -15,36 +15,56 @@ int main(int argc, char** argv)
 	resize(imgsrc,resized_img, Size(imgsrc.cols*1.5,imgsrc.rows*1.5));
 	Size img_size=resized_img.size();
 	Size size=imgsrc.size();
+	Size cropped_size(600,800);
 	
-	//Destination image initialized as blank/empty canvas i.e. a black screen	
+	//Destination images initialized as blank/empty canvas i.e. a black screen	
 	Mat img_dst = Mat::zeros(img_size,CV_32F);
+	Mat img_dst2 = Mat::zeros(img_size,CV_32F);
 
 	//A vector of points to be selected by mouse input
 	vector<Point2f> pts_src;
-	pts_src.push_back(Point2f(0,0));
-	pts_src.push_back(Point2f(0,img_size.height-1));
-	pts_src.push_back(Point2f(img_size.width-1,img_size.height-1));
-	pts_src.push_back(Point2f(img_size.width-1,0));
+	pts_src.push_back(Point2f(1500,400));
+	pts_src.push_back(Point2f(900,img_size.height-200));
+	pts_src.push_back(Point2f(2200,img_size.height-200));
+	pts_src.push_back(Point2f(2000,400));
+	
+	//vector<Point2f> pts_src;
+	//pts_src.push_back(Point2f(0,0));
+	//pts_src.push_back(Point2f(0,img_size.height-1));
+	//pts_src.push_back(Point2f(img_size.width-1,img_size.height-1));
+	//pts_src.push_back(Point2f(img_size.width-1,0));
+	
 
-	//A vector of pre-defined points of img_dst
+	//A vector of pre-defined points of img_dst for displaying the warped image
 	vector<Point2f> pts_dst;
-	pts_dst.push_back(Point2f(0,0));
-	pts_dst.push_back(Point2f(636,img_size.height-1));
-	pts_dst.push_back(Point2f(img_size.width-637,img_size.height-1));
-	pts_dst.push_back(Point2f(img_size.width-1,0));
+	pts_dst.push_back(Point2f(1500,400));
+	pts_dst.push_back(Point2f(1200,img_size.height-200));
+	pts_dst.push_back(Point2f(2000,img_size.height-200));
+	pts_dst.push_back(Point2f(2000,400));
+
+	//A vector of pre-defined points of img_dst2 for displaying the cropped image
+	vector<Point2f> pts_dst2;
+	pts_dst2.push_back(Point2f(0,0));
+	pts_dst2.push_back(Point2f(0,cropped_size.height-1));
+	pts_dst2.push_back(Point2f(cropped_size.width-1,cropped_size.height-1));
+	pts_dst2.push_back(Point2f(cropped_size.width-1,0));
 
 	//Finding warped image
-	Mat h = findHomography(pts_src,pts_dst);
-	warpPerspective(resized_img,img_dst,h,img_size);
+	Mat h1 = findHomography(pts_src,pts_dst);
+	Mat h2 = findHomography(pts_src,pts_dst2);
+	warpPerspective(resized_img,img_dst,h1,img_size);
+	warpPerspective(resized_img,img_dst2,h2,cropped_size);
 
 	//Displaying image(s)
 	imshow("Resized Image",resized_img);
 	imshow("Warped Image",img_dst);
+	imshow("Cropped Image",img_dst2);
 	waitKey(0);
 
 	//Saving the image
 	imwrite("resized.jpg",resized_img);
 	imwrite("warped.jpg",img_dst);
+	imwrite("cropped.jpg",img_dst2);
 	
 	return 0;
 }
