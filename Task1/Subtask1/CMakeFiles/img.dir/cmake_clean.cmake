10b4e812c058c1ee2dc2932d3586b37f3bcb6904
Shrey <pshrey795@gmail.com>
2021-02-24 20:35:55 +0530

    Added cmake files

---
+++ Task1/Subtask1/CMakeFiles/img.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/img.dir/img.cpp.o"
+  "img"
+  "img.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/img.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Task1/Subtask1/img.cpp
+++ Task1/Subtask1/img.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace cv;
 
-
+//External Data Structure to store user inputs
 struct img_points{
     Mat img;
     vector<Point2f> points;
@@ -14,15 +14,26 @@ struct img_points{
 void handle_clicks(int event,int x,int y,int flags,void* data){
 
 	Scalar color=Scalar(0,255,0);
+	
+	//User selected point
 	Point selected=Point(x,y);
     
+    	//Detecting left mouse click
    	if (event==EVENT_LBUTTONDOWN){
+   	
         	img_points* cur_data=(img_points*) data; 
+        	
+        	//Circle to be displayed at the selected point
         	circle((cur_data->img),selected,4,color,-1,FILLED);
+        	
+        	//Displaying updated image
         	imshow("Image",cur_data->img);
         	int sz=(cur_data->points).size();
 
+		//Checking for number of user clicks
         	if (sz>0){
+        	
+        		//Displaying line to show the bounding box
             		line( cur_data->img,(cur_data->points)[sz-1],selected,Scalar(255,255,255),1,LINE_8);
             		imshow("Image",cur_data->img);
         	}
@@ -44,40 +55,60 @@ int main(int argc, char** argv)
 	//Initializing Image
 	Mat imgsrc = imread(string(argv[1]),IMREAD_GRAYSCALE);
 	
+	
 	//Resizing Image
 	Mat resized_img;
 	resize(imgsrc,resized_img, Size(imgsrc.cols*1.5,imgsrc.rows*1.5));
-	Size img_size=resized_img.size();
-	Size size=imgsrc.size();
+	
 	
 	//These values depend upon the display resolution of the machine used 
 	Size cropped_size(600,800);
+	Size img_size=resized_img.size();
+	
 	
 	//Destination images initialized as blank/empty canvas i.e. a black screen	
 	Mat img_dst = Mat::zeros(img_size,CV_32F);
 	Mat img_dst2 = Mat::zeros(img_size,CV_32F);
+	
 	
 	//Clone of the original image to avoid mouse clicks and bounding box in warped and cropped images
 	Mat to_be_displayed = resized_img.clone();
 	img_points userdata;
 	userdata.img=to_be_displayed;
 	imshow("Image",to_be_displayed);
+	
+	
+	
+	//Calling procedure to take input mouse clicks
 	setMouseCallback("Image",handle_clicks,&userdata);
 	waitKey(0);
 	
-	//A vector of pre-defined points of img_dst for displaying the warped image
+	
+	
+	//A vector of points of derived points on img_dst for displaying the warped image
 	vector<Point2f> pts_dst;
 	float x1 = (userdata.points[0].x+userdata.points[1].x)/2;
 	float y2 = (userdata.points[1].y+userdata.points[2].y)/2;
 	float x2 = (userdata.points[2].x+userdata.points[3].x)/2;
-	//float y1 = (userdata.points[3].y+userdata.points[0].y)/2;
 	float y1 = y2-3*(x2-x1)/2;
-	pts_dst.push_back(Point2f(x1+100,y1+100));
-	pts_dst.push_back(Point2f(x1+100,y2));
-	pts_dst.push_back(Point2f(x2-100,y2));							
-	pts_dst.push_back(Point2f(x2-100,y1+100));
-	//These values also depend upon the display resolution of the machine used 
+	//Generalisation for point selection
+	if (y1<0){
+		y2+=abs(y1);
+		y1=0.0;
+		if (y2>img_size.height){
+			float dif=y2-img_size.height;
+			y2=img_size.height;
+			dif*=0.67;
+			x1+=dif/2;
+			x2-=dif/2;
+		}
+	}
+	pts_dst.push_back(Point2f(x1,y1));
+	pts_dst.push_back(Point2f(x1,y2));
+	pts_dst.push_back(Point2f(x2,y2));							
+	pts_dst.push_back(Point2f(x2,y1));
 
+	
 
 	//A vector of pre-defined points of img_dst2 for displaying the cropped image
 	vector<Point2f> pts_dst2;
@@ -87,18 +118,21 @@ int main(int argc, char** argv)
 	pts_dst2.push_back(Point2f(cropped_size.width-1,0));
 	
 
-	//Finding warped image
+	//Obtaining warped image
 	Mat h1 = findHomography(userdata.points,pts_dst);
-	Mat h2 = findHomography(userdata.points,pts_dst2);
 	warpPerspective(resized_img,img_dst,h1,img_size);
+	
+	//Obtaining cropped image
+	Mat h2 = findHomography(userdata.points,pts_dst2);
 	warpPerspective(resized_img,img_dst2,h2,cropped_size);
 
 	//Displaying image(s)
 	imshow("Warped Image",img_dst);
+	waitKey(0);
 	imshow("Cropped Image",img_dst2);
 	waitKey(0);
 
-	//Saving the image
+	//Saving the image in the directory
 	imwrite("resized.jpg",resized_img);
 	imwrite("warped.jpg",img_dst);
 	imwrite("cropped.jpg",img_dst2);
