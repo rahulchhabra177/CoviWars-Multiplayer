8979873b1126333b4ae6db88db63618f6c65f4e1
Rahul <chhabrarahul882@gmail.com>
2021-03-23 22:26:33 +0530

    Some debugging remaining

--- Task1/Subtask3/video_m1.cpp
+++ Task1/Subtask3/video_m1.cpp
@@ -99,7 +99,9 @@ int main(int argc,char** argv)
 			Mat currentImg = initialImg;
 			
 			//Current frame number
-			int frameNo = 1;
+			int frameNo = 1,x;
+			cout<<"Please Enter the value of x\n";
+			cin>>x;
 			
 			//Queue density and Dynamic density values for the last frame. Note that we 
 			//don't calculate these values for the first frame, as we have taken the first 
@@ -114,68 +116,70 @@ int main(int argc,char** argv)
  				//Indicates if we reached the end of the video i.e. no more frames to 
  				//process
  				bool notOver = cap.read(frame);
- 				
+
  				//Exiting the loop if video is over
  				if(!notOver){
  					break;
  				}
- 				
  				//Manipulating the current frame so that it can be operated with the 
  				//reference frame
  				resize(frame,frame,Size(1.5*img_size.width,1.5*img_size.height));
  				cvtColor(frame,frame,COLOR_BGR2GRAY);
  				warpPerspective(frame,frame,h,cropped_size);
+ 				if (frameNo%x==0){
+	 				
 
-				//queueImg = Image showing the queued traffic of the current frame
-				//diffImg = Image showing the moving traffic of the current frame  
-				Mat diffImg;
-				Mat queueImg;
+					//queueImg = Image showing the queued traffic of the current frame
+					//diffImg = Image showing the moving traffic of the current frame  
+					Mat diffImg;
+					Mat queueImg;
+					
+					//queueImg can be obtained by background subtraction, i.e. by subtracting 
+					//the background/reference frame from the current frame.
+					absdiff(frame,initialImg,queueImg);
+					
+					//diffImg can be obtained by subtraction of consecutive frames, i.e.
+					//by subtracting last frame(stored in currentImg) from the current frame
+					absdiff(frame,currentImg,diffImg);
+
+					//Removing distortions(noise) from both the images by applying a 
+					//threshold filter and a Gaussian blur
+					threshold(queueImg,queueImg,50,255,0); 
+					GaussianBlur(queueImg,queueImg,Size(45,45),10,10);
+					threshold(diffImg,diffImg,20,255,0); 
+					GaussianBlur(diffImg,diffImg,Size(45,45),10,10); 
+
+					//This block of code applies a filter to the queue density and dynamic 
+					//density values to reduce fluctuations and distortions in adjacent 
+					//values to obtain a "relatively" smooth graph
+					if(frameNo == 1){
+					     qDensity = (1-black_density(queueImg));
+					     dDensity = (1-black_density(diffImg));
+					}else{
+					     float q = 1-black_density(queueImg);
+					     float d = 1-black_density(diffImg);
+					     
+					     //If the density values of consecutive frames differ by more than
+					     //0.2, we extrapolate the last value, else we accept the density
+					     //values of current frame. 
+					     if(abs(q-qDensity)<=0.1){
+					     	qDensity = q;
+					     }
+					     if(abs(d-dDensity)<=0.1){
+					     	dDensity = d;
+					     }
+					}
+					
+					//Writing the frame number and density values in the command line
+					//fstream myfile("out.txt",std::ios_base::app);
+					//myfile<<frameNo<<","<<(qDensity)<<","<<(dDensity)<<endl;
 				
-				//queueImg can be obtained by background subtraction, i.e. by subtracting 
-				//the background/reference frame from the current frame.
-				absdiff(frame,initialImg,queueImg);
-				
-				//diffImg can be obtained by subtraction of consecutive frames, i.e.
-				//by subtracting last frame(stored in currentImg) from the current frame
-				absdiff(frame,currentImg,diffImg);
-
-				//Removing distortions(noise) from both the images by applying a 
-				//threshold filter and a Gaussian blur
-				threshold(queueImg,queueImg,50,255,0); 
-				GaussianBlur(queueImg,queueImg,Size(45,45),10,10);
-				threshold(diffImg,diffImg,20,255,0); 
-				GaussianBlur(diffImg,diffImg,Size(45,45),10,10); 
-
-				//This block of code applies a filter to the queue density and dynamic 
-				//density values to reduce fluctuations and distortions in adjacent 
-				//values to obtain a "relatively" smooth graph
-				if(frameNo == 1){
-				     qDensity = (1-black_density(queueImg));
-				     dDensity = (1-black_density(diffImg));
-				}else{
-				     float q = 1-black_density(queueImg);
-				     float d = 1-black_density(diffImg);
-				     
-				     //If the density values of consecutive frames differ by more than
-				     //0.2, we extrapolate the last value, else we accept the density
-				     //values of current frame. 
-				     if(abs(q-qDensity)<=0.1){
-				     	qDensity = q;
-				     }
-				     if(abs(d-dDensity)<=0.1){
-				     	dDensity = d;
-				     }
 				}
-				
-				//Writing the frame number and density values in the command line
-				//fstream myfile("out.txt",std::ios_base::app);
-				//myfile<<frameNo<<","<<(qDensity)<<","<<(dDensity)<<endl;
-				cout<<frameNo<<","<<(qDensity)<<","<<(dDensity)<<endl;
-
 				//Iterating through the frames
-				currentImg = frame;
+				currentImg = frame;	
+				cout<<frameNo<<","<<(qDensity)<<","<<(dDensity)<<endl;
 				frameNo++;
- 			}
+	 		}
  		
  		}
 		
