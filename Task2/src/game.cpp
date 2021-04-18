8a73cd5c23f4f78381ffcd636bebee39492a5a23
Rahul <chhabrarahul882@gmail.com>
2021-04-18 22:06:10 +0530

    Added Game object file

--- Task2/src/game.cpp
+++ Task2/src/game.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <SDL2/SDL_image.h>
 #include "Texture.h";
+#include "pacman.h";
 using namespace std;
-
-
+int rooot=0;
+pacman * mainplayer=nullptr,*enemy1=nullptr;
+int SCREENWIDTH=400;
+int SCREENHEIGHT=300;
 
 Game::Game(){
 
@@ -44,31 +47,65 @@ void Game::init(char* title, int xcor,int ycor,int width_window,int height_windo
 				
 				else{
 				running=true;
+				mainplayer=new pacman("./../assets/hero.bmp",renderer,SCREENWIDTH/2,SCREENHEIGHT,false);
+				enemy1=new pacman("/home/rahul/Downloads/try.bmp",renderer,0,0,true);
+				
     			}
 			}
 
 		}
 	}
 	else{
-		cout<<"Shit!!\n";
 		running=false;
 	}
 }
 
 
 void Game::handle_event(){
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type){
+		case SDL_QUIT:	
+			running=false;
+			break;
+		case SDL_KEYDOWN:
+			switch ( event.key.keysym.sym ){
+				case SDLK_UP:
+					mainplayer->y--;
+					break;
+				case SDLK_DOWN:
+					mainplayer->y++;
+					break;
+				case SDLK_RIGHT:
+					mainplayer->x++;
+					break;
+				case SDLK_LEFT:
+					mainplayer->x--;
+					break;
+				default:
+					break;					
+			}	
+		default:
+			break;
+	}
 
 }
 
 
 void Game::process(){
-	cnt++;
-	cout<<cnt<<"\n";
+	mainplayer->update();
+	enemy1->update();
+	
+
 }
 
 void Game::render(){
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer,backg,NULL,NULL);
+	mainplayer->render();
+	enemy1->render();
+	
+
 	SDL_RenderPresent(renderer);
 }
 
---
+++ Task2/src/pacman.cpp
@@ -0,0 +1,39 @@
+#include "Texture.h";
+#include "pacman.h";
+#include "game.hpp";
+using namespace std;
+int temp=0;
+pacman::pacman(char * path,SDL_Renderer* rendere,int init_x,int init_y,bool isenem){
+	character=Texture::LoadT(path,rendere);
+	renderer=rendere;
+	x=init_x;
+	y=init_y;
+	isenemy=isenem;
+
+}
+
+
+void pacman::update(){
+
+	
+	cout<<x<<" "<<y<<"\n";
+	// srcr.h=32;
+	// srcr.w=32;
+	temp=(temp+1)%10;
+	dstr.h=height;
+	dstr.w=width;
+	if (isenemy and temp%2==0){y+=temp/9;temp+=18;}
+	if (isenemy and temp%2==1){x+=temp/9;temp+=21;}
+	dstr.x=x;
+	dstr.y=y;
+
+}
+
+
+void pacman::render(){
+
+
+SDL_RenderCopy(renderer,character,NULL,&dstr);
+
+}
+
